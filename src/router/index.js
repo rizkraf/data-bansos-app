bb213bc48bce160539c3cf81bd71cc3b02f861e4
Rizky Rafi Azhara <azhararizkyrafi@gmail.com>
2022-04-06 15:44:22 +0700

    fix router routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import FormView from '../views/FormView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
@@ -12,10 +13,7 @@ const router = createRouter({
     {
       path: '/formulir',
       name: 'formulir',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/FormView.vue')
+      component: FormView
     }
   ]
 })
